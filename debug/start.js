0d489c0af5c26e01e83e959236503ccfa29c19d7
qianqian <qianqian@xunfeng.cn>
2025-02-08 11:12:10 +0800

    feat: 支持不同命令打包不同的mode

--- debug/start.js
+++ debug/start.js
@@ -3,8 +3,9 @@ const webpack = require("../lib/index.js");
 // 引入上面写的webpack配置对象
 const config = require("./webpack.config");
 // 创建一个compiler对象
-debugger;
-const compiler = webpack(config);
+
+// 传入环境变量
+const compiler = webpack(config({ WEBPACK_BUILD: process.env.WEBPACK_BUILD }));
 
 // 执行compiler.run方法开始编译代码，回调方法用于反馈编译的状态
 compiler.run((err, stats) => {
@@ -13,18 +14,18 @@ compiler.run((err, stats) => {
 		return;
 	}
 
-	// 由于现在是多配置，stats是一个MultiStats对象
-	const statsArray = stats.stats;
+	// 由于可能是多配置，需要判断stats是否是MultiStats
+	const statsArray = stats.stats || [stats];
 	statsArray.forEach((stat, index) => {
+		const mode =
+			process.env.WEBPACK_BUILD || (index === 0 ? "development" : "production");
+		console.log(`\n构建 ${mode} 模式结果：`);
 		console.log(
-			`\n构建 ${index === 0 ? "development" : "production"} 模式结果：`
+			stat.toString({
+				chunks: false, // 使输出更清晰
+				colors: true // 让输出带有颜色
+			})
 		);
-		// console.log(
-		// 	stat.toString({
-		// 		chunks: false, // 使输出更清晰
-		// 		colors: true // 让输出带有颜色
-		// 	})
-		// );
 	});
 
 	// 完成后调用close
